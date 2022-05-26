1d181c48d7ea4712e728a175ba4fa3cc19243c98
Brikane <97720647+Brikane@users.noreply.github.com>
2022-05-26 11:09:05 -0500

    Update AFTLiteButtonsOnAmazonSearch.user.js

--- AFTLiteButtonsOnAmazonSearch.user.js
+++ AFTLiteButtonsOnAmazonSearch.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AFTLiteButtonsOnAmazonSearch
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  adds buttons to open amazon search items in AFTLite
 // @author       brikane
 // @match        https://www.amazon.com/*
@@ -18,6 +18,8 @@ var asinSplit = "dp/";
 var asinLength = 10;
 var baseURL = "https://aftlite-na.amazon.com/inventory/view_inventory_for_asin?asin=";
 
+var linkList = [];
+//var buttonList;
 //---------------------- MAIN
 (function() {
     'use strict';
@@ -28,17 +30,21 @@ function idASINS(){
     var elList = document.getElementsByClassName(asinTag);
 
     console.log("El Count:" , elList.length);
+    var button_Search ="";
     for (let index = 0; index < elList.length; index++) {
         var element = elList[index].getAttribute("href");
         var asinStr = element.split(asinSplit).pop().substring(0,asinLength);
         // console.log("Link:" + asinStr);
         var openURL = baseURL + asinStr;
-        var button_Search = createButton("AFTLite: " + asinStr);
+        linkList.push(openURL);
+        button_Search = createButton("AFTLite: " + asinStr);
+        console.log("Link:" + openURL);
         button_Search.onclick = function () {
-        window.open(openURL);
-    };
-
-    elList[index].parentElement.append(button_Search);
+            console.log("Link:" + linkList[index]);
+            window.open(linkList[index]);
+        };
+        //buttonList.append(button_Search);
+        elList[index].parentElement.append(button_Search);
     }
 }
 
