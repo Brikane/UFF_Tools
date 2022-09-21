56ee12689bbf47a096e194e4c080df37fa5af3c1
Brikane <97720647+Brikane@users.noreply.github.com>
2022-09-21 12:50:21 -0500

    Create DailyDetailHelper.user.js

---
+++ DailyDetailHelper.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Daily Detail Help
+// @namespace    https://github.com/Brikane/UFF_Tools
+// @version      1.0
+// @description  Add a lookup history link to the daily detail page.
+// @author       grajef@ re-post @brikane
+// @match        https://aftlite-na.amazon.com/labor_tracking/view_daily_detail*
+// @match        https://aftlite-eu.amazon.com/labor_tracking/view_daily_detail*
+// @match        https://aftlite-nrt.amazon.com/labor_tracking/view_daily_detail*
+// @downloadURL  https://github.com/Brikane/UFF_Tools/raw/master/DailyDetailHelper.user.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var table = document.getElementById("dailyReportTable");
+    var row = 0;
+    for(var i = 1; row = table.rows[i]; i++) {
+        row.cells[0].innerHTML = "<a href=\"https://aftlite-na.amazon.com/labor_tracking/lookup_history?user_name=" + row.cells[0].innerHTML + "\">" + row.cells[0].innerHTML + "</a>"
+    }
+})();
