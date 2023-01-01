45de6fe6ff0ee7e3bc0d85e6558bd604d3857fdc
Brikane <97720647+Brikane@users.noreply.github.com>
2023-01-01 03:43:21 -0600

    Update BATCHING_MONITOR_COMO.user.js
    fixed download and update links

--- RePosts/BATCHING_MONITOR_COMO.user.js
+++ RePosts/BATCHING_MONITOR_COMO.user.js
@@ -5,8 +5,8 @@
 // @description  MONITOR BATCHING TASKS: SET RECOMMENDED BATCHERS. SET TASKS PER BATCHER. SHOW ELAPSED TIME (ET) PER TASK W/ 40 SEC BUFFER.
 // @author       jeyartil repost brikane
 // @match        https://como-operations-dashboard-iad.iad.proxy.amazon.com/store/*/dash
-// @downloadURL  https://raw.githubusercontent.com/JeysonArtiles/amzn/master/batchingMonitor_como.user.js
-// @updateURL    https://raw.githubusercontent.com/JeysonArtiles/amzn/master/batchingMonitor_como.user.js
+// @downloadURL  https://raw.githubusercontent.com/Brikane/UFF_Tools/RePosts/master/batchingMonitor_como.user.js
+// @updateURL    https://raw.githubusercontent.com/Brikane/UFF_Tools/RePosts/master/batchingMonitor_como.user.js
 // @icon         https://www.google.com/s2/favicons?domain=amazon.com-update-test
 // @grant        none
 // @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
