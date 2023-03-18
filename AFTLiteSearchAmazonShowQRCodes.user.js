8d6dc47668e265d235a682329b1ea655a0930272
Brikane <97720647+Brikane@users.noreply.github.com>
2023-03-17 23:45:19 -0500

    Deleted un needed QR codes

--- AFTLiteSearchAmazonShowQRCodes.user.js
+++ AFTLiteSearchAmazonShowQRCodes.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AFTLiteSearchAmazonShowQRCodes
 // @namespace    https://github.com/Brikane/UFF_Tools
-// @version      2.0
+// @version      2.1
 // @description  Shows Location and ASIN QR Codes
 // @author       brikane@
 // @match        https://aftlite-na.amazon.com/inventory/view_inventory_for_asin*
 // @match        https://aftlite-portal.amazon.com/inventory/view_inventory_for_asin_display*
-// @downloadURL  https://github.com/Brikane/UFF_Tools/raw/master/AFTLiteSearchAmazonShowQRCodes.user.js
-// @updateURL    https://github.com/Brikane/UFF_Tools/raw/master/AFTLiteSearchAmazonShowQRCodes.user.js
+// @downloadURL  https://github.com/Brikane/UFF_Tools/raw/master/AFTLiteShowQRCodes.user.js
+// @updateURL    https://github.com/Brikane/UFF_Tools/raw/master/AFTLiteShowQRCodes.user.js
 // @grant        none
 // ==/UserScript==
 // Vars
@@ -22,6 +22,8 @@ var asinLength = 10;
 
 var localTag = "table";
 var cellIndex = 0;
+
+var endTableRowIgnore = 4;
 
 // Main
 (function() {
@@ -48,8 +50,12 @@ function addASIN_QRCode(){
 function addLocationQRCodes(){
     console.log("FIndingLocations...");
     var table = document.getElementsByTagName(localTag)[1];
+    var numRows = table.rows.length;
     for(let row of table.rows) {
-        if (row.rowIndex > 0   ){
+        if (row.rowIndex > numRows-endTableRowIgnore){
+            var newCell = row.insertCell(cellIndex);
+            newCell.appendChild(document.createTextNode(" "));
+        } else if (row.rowIndex > 0   ){
             let cell = row.cells[0];
             var loc = cell.innerHTML.split("[")[0].trim();
             loc = loc.slice(loc.lastIndexOf('>')+1,loc.length);
